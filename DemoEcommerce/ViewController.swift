5bc337613905448cade5592779c7549da9a5ced5
Ameya Gondhalekar <ameya.g@peopleinteractive.in>
2018-09-25 09:39:32 +0530

    Initial Commit

---
+++ DemoEcommerce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoEcommerce
+//
+//  Created by Swati Gondhalekar on 25/09/18.
+//  Copyright © 2018 Ameya Gondhalekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
